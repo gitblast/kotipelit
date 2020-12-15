55aad9038043dd6aafa3ecff1d99a0c871fc0c35
ArtsiBartsi <arto@spektaakkeli.com>
2020-12-15 17:34:52 +0200

    New layout initiated, previous player hints added

--- src/components/GameLobby.tsx
+++ src/components/GameLobby.tsx
@@ -4,13 +4,22 @@ import { v4 as uuidv4 } from 'uuid';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { useParams } from 'react-router-dom';
 import gameService from '../services/games';
-import { Fab, Paper, Typography, Grid } from '@material-ui/core';
+import {
+  Fab,
+  Paper,
+  Typography,
+  Grid,
+  GridList,
+  GridListTile,
+  GridListTileBar,
+} from '@material-ui/core';
 import { LobbyGame, LobbyGamePlayer } from '../types';
 import logger from '../utils/logger';
 import Loader from './Loader';
 import { capitalize } from 'lodash';
 import { format } from 'date-fns';
 import fiLocale from 'date-fns/locale/fi';
+import userImg from '../assets/images/user.png';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -18,19 +27,47 @@ const useStyles = makeStyles((theme: Theme) =>
       margin: theme.spacing(2),
       padding: theme.spacing(2),
     },
-    playerBox: {
-      marginTop: theme.spacing(2),
-      marginBottom: theme.spacing(2),
-    },
     infoBox: {
       marginTop: theme.spacing(2),
-      marginBottom: theme.spacing(2),
+      marginLeft: theme.spacing(2),
+    },
+    // flexin: {
+    //   display: 'flex',
+    //   alignItems: 'center',
+    //   justifyContent: 'space-evenly',
+    // },
+
+    reserveBtn: {
+      padding: theme.spacing(4),
+      margin: theme.spacing(2),
+      color: 'white',
+      backgroundColor: 'rgb(78 156 63)',
+    },
+    gridTile: {
+      backgroundColor: 'grey',
+      border: 'solid white',
+    },
+    titleBar: {
+      backgroundColor: 'rgba(0, 0, 0, 0.9)',
+      height: 40,
     },
     freeSlotText: {
-      color: 'green',
+      color: 'white',
     },
     bookedText: {
       color: 'red',
+    },
+    sectionB: {
+      marginTop: 150,
+      textAlign: 'center',
+      color: 'grey',
+      [theme.breakpoints.down('xs')]: {
+        marginTop: 30,
+      },
+    },
+    image: {
+      width: '100%',
+      height: 'auto',
     },
   })
 );
@@ -132,47 +169,80 @@ const GameLobby: React.FC<GameLobbyProps> = () => {
   };
 
   return (
-    <Paper className={classes.container}>
+    <Paper elevation={0} className={classes.container}>
       {error && <Typography color="error">{error}</Typography>}
       {game ? (
         <>
-          <Typography variant="h4">{`Tervetuloa pelaamaan ${capitalize(
-            game.type
-          )}a!`}</Typography>
-          <Grid container>
-            <Grid item xs={12} sm={6} className={classes.infoBox}>
-              <Typography variant="h6">{`Peli alkaa ${format(
-                new Date(game.startTime),
-                'd. MMMM HH:mm',
-                {
-                  locale: fiLocale,
-                }
-              )}`}</Typography>
-              {game.price !== 0 && (
-                <Typography variant="h6">{`Pelin hinta on ${game.price} €`}</Typography>
-              )}
-              <Typography variant="h6">{`Peli-illan järjestää ${game.hostName}`}</Typography>
+          <Grid container spacing={3}>
+            <Grid item sm={1}></Grid>
+            <Grid item xs={12} sm={5}>
+              <Typography variant="h4">{`Tervetuloa pelaamaan ${capitalize(
+                game.type
+              )}a!`}</Typography>
+              <div className={classes.infoBox}>
+                <Typography variant="h6">{`Peli alkaa ${format(
+                  new Date(game.startTime),
+                  'd. MMMM HH:mm',
+                  {
+                    locale: fiLocale,
+                  }
+                )}`}</Typography>
+                {game.price !== 0 && (
+                  <Typography variant="h6">{`Pelin hinta on ${game.price} €`}</Typography>
+                )}
+                <Typography variant="h6">{`Peli-illan järjestää ${game.hostName}`}</Typography>
+                <Fab
+                  className={classes.reserveBtn}
+                  variant="extended"
+                  onClick={reserveSpot}
+                  disabled={!reservationId || !gameID}
+                >
+                  <Typography>Varaa paikka</Typography>
+                </Fab>
+              </div>
             </Grid>
 
-            <Grid item xs={12} sm={6} className={classes.playerBox}>
-              <Typography>Pelaajat:</Typography>
-              {game.players.map((player, index) => {
-                return (
-                  <Typography key={index}>
-                    <span>{`${index + 1}. `}</span>
-                    {getLabel(player)}
-                  </Typography>
-                );
-              })}
+            <Grid item xs={12} sm={5}>
+              <GridList cellHeight={160} cols={3}>
+                {game.players.map((player, index) => {
+                  return (
+                    <GridListTile
+                      key={index}
+                      cols={1}
+                      className={classes.gridTile}
+                    >
+                      <img className={classes.image} src={userImg} alt="user" />
+                      {/* <span>{`${index + 1}. `}</span> */}
+
+                      <GridListTileBar
+                        className={classes.titleBar}
+                        subtitle={getLabel(player)}
+                      />
+                    </GridListTile>
+                  );
+                })}
+              </GridList>
+            </Grid>
+            <Grid item sm={1}></Grid>
+          </Grid>
+          <Grid container className={classes.sectionB} spacing={5}>
+            {/* Toistaa itseään, tee mahd. objekti jossa koottuja vihjeitä, vaihtele näkymää */}
+            <Grid item sm={4}>
+              <Typography variant="h6">
+                "Hautajaishuijaus, johon et halua sijoittaa."
+              </Typography>
+            </Grid>
+            <Grid item sm={4}>
+              <Typography variant="h6">
+                "Anthony Hopkinsin lemmikin oikeudellinen asema."
+              </Typography>
+            </Grid>
+            <Grid item sm={4}>
+              <Typography variant="h6">
+                "Löytyy tähtimerkeistä ja Espanjasta."
+              </Typography>
             </Grid>
           </Grid>
-          <Fab
-            variant="extended"
-            onClick={reserveSpot}
-            disabled={!reservationId || !gameID}
-          >
-            Varaa paikka
-          </Fab>
         </>
       ) : (
         <Loader msg={'Ladataan...'} spinner />

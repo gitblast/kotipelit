eb438a551c3bc3d3a5495f19f9101126a3a1c69e
Jarkko Mämmelä <jarkko.poker@gmail.com>
2021-01-14 15:47:27 +0200

    script to list current words

---
+++ scripts/list_words.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+
+const fs = require('fs');
+const path = require('path');
+
+const Word = require('../dist/models/word').default;
+const connection = require('../dist/utils/connection').default;
+
+const listWords = async () => {
+  try {
+    connection.connect(process.env.MONGODB_URI);
+
+    console.log('connected to db');
+    console.log('fetching words...');
+    console.log('');
+
+    const wordsInDb = await Word.find({});
+
+    const wordsInDbAsList = wordsInDb.map((w) => w.word);
+
+    console.log(wordsInDbAsList.length, 'words found.');
+
+    const date = new Date();
+
+    const fileName = `words-${new Date().getDate()}-${
+      new Date().getMonth() + 1
+    }.txt`;
+
+    console.log('writing to', fileName, '...');
+    console.log('');
+
+    fs.writeFileSync(
+      path.join(__dirname, fileName),
+      wordsInDbAsList.join('\n')
+    );
+
+    console.log('done');
+  } catch (e) {
+    console.error('error listing words:', e.message);
+  } finally {
+    await connection.close();
+  }
+};
+
+listWords();

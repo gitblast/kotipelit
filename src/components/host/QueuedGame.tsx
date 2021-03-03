6a0b501b9ab18442369aa9087142b46f2a156725
ArtsiBartsi <arto@spektaakkeli.com>
2021-03-03 18:36:38 +0200

    Color tweaks

--- src/components/host/QueuedGame.tsx
+++ src/components/host/QueuedGame.tsx
@@ -45,9 +45,6 @@ const useStyles = makeStyles((theme: Theme) =>
     avatarStyle: {
       background: theme.palette.secondary.main,
     },
-    cardHeaders: {
-      color: theme.palette.secondary.light,
-    },
     playerRow: {
       display: 'flex',
       justifyContent: 'space-between',
@@ -119,14 +116,20 @@ const QueuedGame: React.FC<QueuedGameProps> = ({ game, username }) => {
   const lobbyButton = () => {
     if (game.status !== GameStatus.FINISHED) {
       return (
-        <Button
-          variant="contained"
-          color="primary"
-          component={Link}
-          to={`/${username}/kutsut/${game.id}`}
-        >
-          Peliaula
-        </Button>
+        <>
+          <Button
+            variant="contained"
+            color="primary"
+            component={Link}
+            to={`/${username}/kutsut/${game.id}`}
+          >
+            Peliaula
+          </Button>
+          <IconButton className={classes.actionIcon} aria-label="copy">
+            {/* This should copy output of getLobbyLink() */}
+            <FileCopyIcon />
+          </IconButton>
+        </>
       );
     }
   };
@@ -196,13 +199,9 @@ const QueuedGame: React.FC<QueuedGameProps> = ({ game, username }) => {
               </Menu>
             </>
           }
-          title={
-            <Typography variant="h3" className={classes.cardHeaders}>
-              {capitalize(game.type)}
-            </Typography>
-          }
+          title={<Typography variant="h3">{capitalize(game.type)}</Typography>}
           subheader={
-            <Typography variant="body1" className={classes.cardHeaders}>
+            <Typography variant="body1">
               {format(new Date(game.startTime), 'd. MMMM HH:mm', {
                 locale: fiLocale,
               })}{' '}
@@ -257,10 +256,7 @@ const QueuedGame: React.FC<QueuedGameProps> = ({ game, username }) => {
         <CardActions disableSpacing className={classes.actions}>
           <div>
             {lobbyButton()}
-            <IconButton className={classes.actionIcon} aria-label="copy">
-              {/* This should copy output of getLobbyLink() */}
-              <FileCopyIcon />
-            </IconButton>
+
             {/* For sharing gameLobby page in social medias */}
             {/* <IconButton className={classes.actionIcon} aria-label="share">
               <ShareIcon />

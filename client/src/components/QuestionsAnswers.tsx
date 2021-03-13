175d65c35e45dc32f492f015646d2bfc4a3f1b98
ArtsiBartsi <arto@spektaakkeli.com>
2021-03-13 12:09:57 +0200

    Content changes

--- client/src/components/QuestionsAnswers.tsx
+++ client/src/components/QuestionsAnswers.tsx
@@ -24,24 +24,27 @@ const QuestionsAnswers = () => {
           Maksaako pelien järjestäminen?
         </Typography>
         <Typography variant="body2" color="initial">
-          Pelien järjestäminen ei maksa mitään. Sen sijaan voit halutessasi
-          asettaa pelaajille hinnan, josta 80% tulee sinulle ja 20% käytetään
-          Kotipelit-sivuston kehittämiseen.
+          Pelien järjestäminen ei maksa mitään. Toistaiseksi myös pelaaminen on
+          täysin ilmaista. Kehitteillä on ominaisuus, jolla pelin juontajana
+          voit halutessasi asettaa pelaajille hinnan, josta 80% tulisi sinulle
+          ja 20% käytetään Kotipelit-sivuston kehittämiseen.
         </Typography>
         <Typography variant="body1" color="initial">
-          Voinko juontaa ystävilleni ilmaisia peli-iltoja?
+          Voinko juontaa ystävilleni jatkossakin ilmaisia peli-iltoja?
         </Typography>
         <Typography variant="body2" color="initial">
-          Peli-iltasi voi myös olla pelaajille ilmaisia ja alkuun näin
-          suosittelemme tekemäänkin. Maksullisissa peleissä on kuitenkin
-          vakaamman yhteyden lisäksi muita lisäominaisuuksia.
+          Peli-iltasi voi jatkossakin olla pelaajille ilmaisia ja alkuun näin
+          suosittelemme tekemäänkin. Maksullisissa peleissä tulee olemaan
+          kuitenkin vakaamman yhteyden lisäksi muitakin lisäominaisuuksia.
         </Typography>
         <Typography variant="body1" color="initial">
           Kuinka järjestän peli-illan?
         </Typography>
         <Typography variant="body2" color="initial">
-          Kun olemme tehneet sinulle käyttäjätilin, näet profiilistasi ohjeet
-          pelien luomiseen ja pisteiden yms. ylläpitämiseen pelien aikana.
+          Kun olemme tehneet sinulle käyttäjätilin, saat luotua pelejä ja
+          kutsuttua kavereita helposti peliin ilmottatumislinkin avulla.
+          Pisteiden päivitys yms. pelin toiminnot videopuhelun aikana ovat
+          helppokäyttöisiä ja apua voi kysyä milloin vaan.
         </Typography>
         <Typography variant="h3" color="initial">
           Pelaajille
@@ -50,25 +53,23 @@ const QuestionsAnswers = () => {
           Tarvitsenko käyttäjätilin pelatakseni?
         </Typography>
         <Typography variant="body2" color="initial">
-          Pelaajat eivät tarvitse käyttäjätiliä, mutta pelin järjestäjällä tulee
+          Pelaajat eivät tarvitse käyttäjätiliä, mutta pelin juontajalla tulee
           olla tili voidaakseen luoda pelejä.
         </Typography>
         <Typography variant="body1" color="initial">
           Miksi maksaisin peli-illasta?
         </Typography>
         <Typography variant="body2" color="initial">
-          80% peli-illan tuotosta menee pelin järjestäjälle, joka voi
-          esimerkiksi olla ystäväsi tai sukulaisesi. Maksullisissa peleissä on
-          myös vakaampi yhteys ja lisäominaisuuksia, mitä ilmaisissa peleissä ei
-          ole.
+          80% peli-illan tuotosta menee pelin juontajalle, joka voi esimerkiksi
+          olla ystäväsi tai sukulaisesi. Maksullisissa peleissä on myös vakaampi
+          yhteys ja lisäominaisuuksia, mitä ilmaisissa peleissä ei ole.
         </Typography>
         <Typography variant="body1" color="initial">
           Miten voin tilata peli-illan ystävilleni, perheelle tai yritykselle?
         </Typography>
         <Typography variant="body2" color="initial">
           Voit ottaa yhteyttä info [at] kotipelit.com ja etsimme teidän
-          ajankohdalle vapaan pelijuontajan. Tilatut pelit ovat aina
-          maksullisia.
+          ajankohdalle vapaan pelijuontajan.
         </Typography>
       </Paper>
     </>

8f567bb998673e1aa991215b6992d78ffbe3cbf8
gitblast <jarkko.mammela@gmail.com>
2021-03-18 18:25:52 +0200

    refactor playeroverlayitems

--- client/src/components/PlayerOverlayItems.tsx
+++ client/src/components/PlayerOverlayItems.tsx
@@ -1,12 +1,14 @@
-import { Checkbox, Grid, Paper, Typography } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import React from 'react';
 import { Animated } from 'react-animated-css';
-import { shallowEqual, useDispatch, useSelector } from 'react-redux';
-import { setClicked } from '../reducers/kotitonni.local.reducer';
+import { useSelector } from 'react-redux';
 import { GameStatus, GameType, RTCParticipant, State } from '../types';
 import logger from '../utils/logger';
+import AnswerBubble from './AnswerBubble';
 import MediaControls from './MediaControls';
+import DevItems from './DevItems';
+import useKotitonniOverlayItems from '../hooks/useKotitonniOverlayItems';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -32,18 +34,7 @@ const useStyles = makeStyles((theme: Theme) =>
       // fills empty space
       flex: '1 1 auto',
     },
-    answerBubble: {
-      // Shapes made with https://bennettfeely.com/clippy/
-      clipPath:
-        'polygon(0% 0%, 100% 0%, 100% 75%, 79% 75%, 80% 99%, 55% 76%, 0% 75%)',
-      position: 'absolute',
-      width: 'fit-content',
-      padding: theme.spacing(1),
-      paddingBottom: theme.spacing(3),
-      margin: theme.spacing(1),
-      display: 'flex',
-      alignItems: 'center',
-    },
+
     pointsAddition: {
       color: 'white',
       backgroundColor: 'rgb(34 110 108)',
@@ -81,88 +72,21 @@ interface PlayerOverlayItemsProps {
 const PlayerOverlayItems: React.FC<PlayerOverlayItemsProps> = ({
   participant,
 }) => {
-  const playerId = participant.id;
   const classes = useStyles();
-  const clickMap = useSelector(
-    (state: State) => state.rtc.localData.clickedMap
-  );
-  const timer = useSelector((state: State) => state.rtc.localData.timer);
-  const dispatch = useDispatch();
+
   const forHost = useSelector((state: State) => state.rtc.self?.isHost);
-  const game = useSelector((state: State) => state.rtc.game);
-  const player = useSelector(
-    (state: State) => state.rtc.game?.players.find((p) => p.id === playerId),
-    shallowEqual
-  );
 
-  const showPointAddition = React.useMemo(() => {
-    if (timer === 0) {
-      return true;
-    }
-
-    const values = Object.values(clickMap);
-
-    return values.some((val) => !!val);
-  }, [clickMap, timer]);
-
-  const checked = !!clickMap[playerId];
-
-  const handleChange = () => {
-    dispatch(setClicked(playerId, !checked));
-  };
-
-  const getAnswer = () => {
-    if (!game || !player || !player.privateData?.answers) {
-      return null;
-    }
-
-    const { turn, round } = game.info;
-
-    const answers = player.privateData.answers[turn];
-
-    if (!answers || !answers[round] || !answers[round].length) {
-      return null;
-    }
-
-    return answers[round];
-  };
-
-  const answerBox = (answer: string) => {
-    return (
-      <Paper className={classes.answerBubble}>
-        <Typography variant="h6">{answer}</Typography>
-        {forHost && answer && (
-          <Checkbox checked={checked} onChange={handleChange} />
-        )}
-      </Paper>
-    );
-  };
+  const {
+    game,
+    player,
+    pointAddition,
+    answer,
+    showPointAddition,
+  } = useKotitonniOverlayItems(participant.id);
 
   if (!game || !player) {
     return null;
   }
-
-  const getPointAddition = (playerId: string, hasTurn: boolean): number => {
-    const playerCount = game.players.length;
-    const correctAnswers = game.players.reduce((sum, next) => {
-      return clickMap[next.id] ? sum + 1 : sum;
-    }, 0);
-
-    switch (correctAnswers) {
-      case playerCount - 1:
-        return hasTurn ? -50 : 0;
-      case 0:
-        return hasTurn ? -50 : 0;
-      case 1:
-        return clickMap[playerId] || hasTurn ? 100 : 0;
-      case 2:
-        return clickMap[playerId] || hasTurn ? 30 : 0;
-      case 3:
-        return clickMap[playerId] || hasTurn ? 10 : 0;
-    }
-
-    return correctAnswers;
-  };
 
   // const getPosition = () => {
   //   const pos = game.players
@@ -173,36 +97,16 @@ const PlayerOverlayItems: React.FC<PlayerOverlayItemsProps> = ({
   //   return pos + 1;
   // };
 
-  const answer = getAnswer();
-
-  const addition = getPointAddition(player.id, !!player.hasTurn);
-
   // handle different game types here
   if (game.type === GameType.KOTITONNI) {
     return (
       <div className={classes.flexCol}>
-        {
-          // eslint-disable-next-line no-undef
-          process && process.env.NODE_ENV === 'development' && (
-            <div style={{ position: 'absolute' }}>
-              {forHost && (
-                <Typography
-                  component="div"
-                  variant="caption"
-                >{`http://localhost:3000/username/${player.privateData.inviteCode}`}</Typography>
-              )}
-
-              {participant.isMe && (
-                <div>
-                  <Typography color="error" variant="h5">
-                    ME
-                  </Typography>
-                </div>
-              )}
-            </div>
-          )
-        }
-        {forHost && showPointAddition && addition !== 0 && (
+        <DevItems
+          forHost={forHost}
+          isMe={participant.isMe}
+          inviteCode={player?.privateData?.inviteCode}
+        />
+        {forHost && showPointAddition && pointAddition !== 0 && (
           <div>
             <Animated
               animationIn="fadeIn"
@@ -211,22 +115,20 @@ const PlayerOverlayItems: React.FC<PlayerOverlayItemsProps> = ({
               isVisible={true}
             >
               <Typography variant="h6" className={classes.pointsAddition}>
-                {addition}
+                {pointAddition}
               </Typography>
             </Animated>
           </div>
         )}
-        {forHost && answer && answerBox(answer)}
+        {forHost && answer && (
+          <AnswerBubble answer={answer} playerId={participant.id} />
+        )}
         {game.status === GameStatus.FINISHED && (
           <div className={classes.positionLabel}>
             {/* Final position expressed later with animations */}
             {/* <Typography variant="h3">{getPosition()}</Typography> */}
           </div>
         )}
-
-        <div className={classes.flex}>
-          <div className={classes.spacer} />
-        </div>
 
         <div className={classes.spacer} />
         <div className={classes.flex}>
